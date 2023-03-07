22ffbb7e3b352311d267c3809e024a47c860765e
Marmin <marmin.dev@gmail.com>
2023-03-07 23:50:14 +0900

    URLconf ended

--- mypost/urls.py
+++ mypost/urls.py
@@ -1,4 +1,24 @@
 from django.urls import path, include
+from mypost import views
 
+app_name = 'mypost'
 urlpatterns = [
+    # /post/
+    path('', views.IndexView.as_view(), name='index'),
+
+    # /post/id/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # /post/all/
+    path('post/all/',views.PostListView.as_view(), name='all'),
+
+    # /post/id/delete/
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+
+    # /post/id/update
+    path('<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+
+    # /post/id/post
+    path('<int:pk>/post/', views.PostCreateView.as_view(), name='post_create')
+
 ]
--- mypost/views.py
+++ mypost/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views import generic
+from mypost.models import Post
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from mypost.forms import PostForm
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'mypost/index.html'
+    context_object_name = 'latest_post_list'
+    def get_queryset(self):
+        return Post.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'mypost/detail.html'
+
+class PostListView(generic.ListView):
+    template_name = 'mypost/all.html'
+    context_object_name = 'all_post_list'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-pub_date')
+
+class PostCreateView(CreateView):
+    # model = Post
+    form_class = PostForm
+    context_object_name = 'post_create'
+    template_name = 'mypost/post_create.html'
+    success_url = reverse_lazy('mypost:index')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    context_object_name = 'post_delete'
+    success_url = reverse_lazy('mypost:index')
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+class PostUpdateView(UpdateView):
+    # model = Post
+    form_class = PostForm
+    context_object_name = 'post_update'
+    template_name = 'mypost/post_update.html'
+    success_url = reverse_lazy('mypost:index')
